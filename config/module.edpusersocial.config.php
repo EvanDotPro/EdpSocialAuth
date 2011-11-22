69ceb41d557fb34126c525044631eec1722e3c6f
Evan Coury <me@evancoury.com>
2011-11-21 19:00:39 -0700

    s/configs/config

---
+++ config/module.edpusersocial.config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * EdpUserSocial Configuration
+ *
+ * If you have a conf.d/ directory set up for your project, you can drop this config file in
+ * it and change the values as you wish.
+ */
+$edpUserSocialSettings = array(
+
+
+
+);
